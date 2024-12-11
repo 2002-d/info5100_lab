b36b3211b196ea0425274ab256e06db1f6977566
2002-d <dasari.y@northeastern.edu>
2024-12-10 21:19:19 -0500

    added final project for car insurance form

--- final/FinalProject/src/View/JFrame.java
+++ final/FinalProject/src/View/JFrame.java
@@ -116,10 +116,6 @@ public class JFrame extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -145,6 +141,7 @@ public class JFrame extends javax.swing.JFrame {
                 new JFrame().setVisible(true);
             }
         });
+    
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
